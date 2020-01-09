7c5b2cb04cc1de7221bfc596ff4f2cda63530409
Brendan Schatz <brendanschatz97@gmail.com>
2020-01-09 12:56:13 -0500

    switched to postgres

--- data/seeds/001_users.js
+++ data/seeds/001_users.js
@@ -4,35 +4,34 @@ const bcrypt = require("bcryptjs");
 
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('newUsers').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('newUsers').insert([
-        {
-          username: "Brendan",
-          password: bcrypt.hashSync("123456", 10),
-          full_name: "Brendan Schatz",
-          email: "brendan@email.com",
-          location: "Michigan",
-          bio: "I can't cook",
+  // return knex('newUsers').del()
+  //   .then(function () {
+  //     // Inserts seed entries
+  return knex('newUsers').insert([
+    {
+      username: "Brendan",
+      password: bcrypt.hashSync("123456", 10),
+      full_name: "Brendan Schatz",
+      email: "brendan@email.com",
+      location: "Michigan",
+      bio: "I can't cook",
 
-        },
-        {
-          username: "admin",
-          password: bcrypt.hashSync("admin", 10),
-          full_name: "Full Name",
-          email: "admin@email.com",
-          location: "California",
-          bio: "I cook food",
-        },
-        {
-          username: "admin2",
-          password: bcrypt.hashSync("admin2", 10),
-          full_name: "Full Name",
-          email: "admin@email.com",
-          location: "California",
-          bio: "I cook food",
-        }
-      ]);
-    });
-};
+    },
+    {
+      username: "admin",
+      password: bcrypt.hashSync("admin", 10),
+      full_name: "Full Name",
+      email: "admin@email.com",
+      location: "California",
+      bio: "I cook food",
+    },
+    {
+      username: "admin2",
+      password: bcrypt.hashSync("admin2", 10),
+      full_name: "Full Name",
+      email: "admin@email.com",
+      location: "California",
+      bio: "I cook food",
+    }
+  ]);
+}
--- knexfile.js
+++ knexfile.js
@@ -15,6 +15,17 @@ module.exports = {
     },
     seeds: {
       directory: "./data/seeds"
+
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
     }
   }
 };
